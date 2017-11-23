55b3f05d759590886ca19c29a017c0c1ec790766
uuuser <1435233930@qq.com>
2017-11-23 11:34:25 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a =1,b=2;
+        if(a>b){
+        System.out.println("Hello World!");
+    }else
+        System.out.println("Welcome to Java");
+}
+
+
